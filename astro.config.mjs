01bbd50476cc109b2ee74775a5e49a12d8677c4b
Thehatsofdean <2834871+thehatsofdean@users.noreply.github.com>
2026-03-17 10:06:53 +0000

    Feat: bundle and chunk assests

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://www.deanweston.co.uk",
+  vite: {
+    build: {
+      minify: true, // or 'esbuild'
+    },
+  },
 });
